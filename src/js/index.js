5fcfd76753a492295d800068a0270271384caeb9
danylyoenko <lockmaster19@gmail.com>
2025-01-17 18:07:32 +0200

    cmplt hw

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// 1 task
+
+const result = 5 + 5 + "5";
+
+console.log(`result: ${result} and type: ${typeof result}`);
+
+// 2 task
+
+const email = "lmaoprostofila@gmail.com";
+
+console.log(
+  `checking sign "@": ${email.includes("@")}, and lenght: ${email.length} words`
+);
+
+// 3 task
+
+const my = "Da";
+const name = "ni";
+const is = "l";
+
+let fullName = my + name + is;
+
+console.log(fullName + " Viktor");
+
+// 4 task
+
+const userName = "Олександро";
+const payment = 300;
+
+console.log(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
